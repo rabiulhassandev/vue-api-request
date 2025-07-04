5dbf56deef4c46a7933da1a971a1f43ac70d1734
RABIUL HASSAN <rabiulhassandev@gmail.com>
2025-07-04 09:45:56 +0800

    get data from Local JSON Server (DB) and loader added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
-  <h1 class="text-center">API Request</h1>
+  <h1 class="text-center text-white py-2 pb-3">API Request</h1>
 
-  <div class="container">
+  <div class="container pb-5">
     <div class="row">
-      <div class="col-md-4" v-for="user in userObj.users" :key="user.id">
-        <div class="card mb-3">
-          <div class="card-body">
-            <h5 class="card-title">{{ user.name }}</h5>
-            <p class="card-text">{{ user.email }}</p>
-            <p class="card-text"><small class="text-muted">{{ user.phone }}</small></p>
+      <div class="col-md-6">
+        <div class="card border-0 bg-white">
+          <h4 class="text-dark text-center py-3">User List (From: jsonplaceholder online)</h4>
+          <div class="row p-2">
+            <!-- Users -->
+            <div class="col-12 d-flex justify-content-center mb-4" v-if="!dataObj.users.isLoading">
+              <Loader />
+            </div>
+            <div class="col-md-6" v-for="user in dataObj.users" :key="user.id" v-else>
+              <div class="card mb-3 border-primary">
+                <div class="card-body">
+                  <h6 class="card-title text-primary">{{ user.name }}</h6>
+                  <p class="card-text">{{ user.email }}</p>
+                  <p class="card-text"><small class="text-muted">{{ user.phone }}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <div class="card border-0 px-2">
+          <h4 class="text-dark text-center py-3">Destination List (From: local JSON)</h4>
+          <div class="d-flex justify-content-center mb-5" v-if="!dataObj.destinations.isLoading">
+            <Loader />
+          </div>
+          <div class="table-responsive" v-else>
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr class="table-primary">
+                  <th>Name</th>
+                  <th>Days</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <!-- Destinations -->
+                <tr v-for="destination in dataObj.destinations" :key="destination.id">
+                  <td>{{ destination.name }}, {{ destination.country }}</td>
+                  <td>{{ destination.days }}</td>
+                  <td>{{ destination.price }}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
@@ -19,31 +58,49 @@
 
 
 <script setup>
-  import axios from 'axios';
-  import { reactive, onMounted } from 'vue';
+import axios from 'axios';
+import { ref, reactive, onMounted } from 'vue';
 
-  const userObj = reactive({
-    users: []
-  })
+let loadingUsers = ref(false);
+let loadingDestinations = ref(false);
+const dataObj = reactive({
+  users: [],
+  destinations: []
+})
 
 
-  onMounted(() => {
+onMounted(() => {
+  // load users
+  loadUsers();
+  // load destinations
+  loadDestinations();
+})
 
-    // get data by Fetch API
-    fetch('https://jsonplaceholder.typicode.com/users')
+
+// load users
+function loadUsers() {
+  dataObj.users.isLoading = false;
+  fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((data) => {
-      userObj.users = data
+      Promise.resolve(setTimeout(() => {
+        dataObj.users = data;
+        dataObj.users.isLoading = true;
+      }, 1000))
     })
+}
 
-    // get data by Axios
-    // axios.get('https://jsonplaceholder.typicode.com/users')
-    // .then((response) => {
-    //   userObj.users = response.data;
-    // })
+// load destinations
+function loadDestinations() {
+  dataObj.destinations.isLoading = false;
 
-
-
-})
+  axios.get('http://localhost:3000/destinations')
+    .then((response) => {
+      Promise.resolve(setTimeout(() => {
+        dataObj.destinations = response.data;
+        dataObj.destinations.isLoading = true;
+      }, 3000))
+    })
+}
 
 </script>
